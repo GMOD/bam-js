c3e2c44df3004cbecdedf1a4423b2303092518ed
Colin <colin.diesh@gmail.com>
2018-11-21 17:16:52 -0500

    Switch to es6-promisify from util.promisify

--- src/localFile.js
+++ src/localFile.js
@@ -1,4 +1,4 @@
-const promisify = require('util.promisify')
+const {promisify} = require('es6-promisify')
 
 // don't load fs native module if running in webpacked code
 const fs = typeof __webpack_require__ !== 'function' ? require('fs') : null // eslint-disable-line camelcase
