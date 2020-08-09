0575aaac21538369ab64dacfcb543db74a428f5e
Colin <colin.diesh@gmail.com>
2020-08-09 02:31:42 -0400

    Use a simpler concept for getHeader

--- src/htsget.ts
+++ src/htsget.ts
@@ -10,15 +10,29 @@ interface HeaderLine {
   value: string
 }
 
+function concat(arr, opts) {
+  return arr.reduce(async (buf: Promise<Buffer>, chunk: any) => {
+    let dat
+    const { url, headers } = chunk
+    if (url.startsWith('data:')) {
+      dat = await unzip(Buffer.from(url.split(',')[1], 'base64'))
+    } else {
+      const res = await fetch(url, { ...opts, headers })
+      const arrayBuffer = await res.arrayBuffer()
+      const buffer = Buffer.from(arrayBuffer)
+      dat = await unzip(buffer, url)
+    }
+    return Buffer.concat([await buf, dat])
+  }, Buffer.alloc(0))
+}
+
 export default class Htsget extends BamFile {
-  // e.g. 'https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12878?referenceName=22&start=16101000&end=16102000&format=BAM'
   private baseUrl: string
 
   private trackId: string
 
   constructor(args: { trackId: string; baseUrl: string }) {
-    // just override bam defaults
-    // @ts-ignore
+    // @ts-ignore override bam defaults
     super({ bamFilehandle: '?', baiFilehandle: '?' })
     this.baseUrl = args.baseUrl
     this.trackId = args.trackId
@@ -30,47 +44,52 @@ export default class Htsget extends BamFile {
     max: number,
     opts: BamOpts = { viewAsPairs: false, pairAcrossChr: false, maxInsertSize: 200000 },
   ) {
-    const url = `${this.baseUrl}/${this.trackId}?referenceName=${chr}&start=${min}&end=${max}&format=BAM`
+    const base = `${this.baseUrl}/${this.trackId}`
+    const url = `${base}?referenceName=${chr}&start=${min}&end=${max}&format=BAM`
     const chrId = this.chrToIndex && this.chrToIndex[chr]
     const result = await fetch(url, { ...opts })
     if (!result.ok) {
       throw new Error(result.statusText)
     }
     const data = await result.json()
-    yield* this._fetchChunkFeatures(data.htsget.urls.slice(1, -1), chrId, min, max, opts)
+    const uncba = await concat(data.htsget.urls.slice(1))
+
+    yield* this._fetchChunkFeatures(
+      [{ buffer: uncba, chunk: { minv: { dataPosition: 0 } } }],
+      chrId,
+      min,
+      max,
+      opts,
+    )
   }
 
   //@ts-ignore
-  async _readChunk({ chunk, opts }: { chunk: unknown; opts: BaseOpts }, signal?: AbortSignal) {
-    const { url, headers } = chunk as { url: string; headers: Record<string, string> }
-    if (url.startsWith('data:')) {
-      console.log('here')
-      return
-    }
-    const res = await fetch(url, { ...opts, signal, headers })
-    const arrayBuffer = await res.arrayBuffer()
-    const buffer = Buffer.from(arrayBuffer)
-    const slice = await unzip(buffer, chunk)
-    const range = parseRange(Number.MAX_SAFE_INTEGER, headers.range)
+  async _readChunk(params: { chunk: Buffer; opts: BaseOpts }, signal?: AbortSignal) {
+    const { chunk, opts } = params
+    const { buffer, chunk: c2 } = chunk
+    // if (url.startsWith('data:')) {
+    //   console.log('here')
+    //   return
+    // }
+    // const res = await fetch(url, { ...opts, signal, headers })
+    // const arrayBuffer = await res.arrayBuffer()
+    // const buffer = Buffer.from(arrayBuffer)
+    // const slice = await unzip(buffer, chunk)
+    // const range = parseRange(Number.MAX_SAFE_INTEGER, headers.range)
 
     //@ts-ignore
-    chunk.minv = { dataPosition: range[0].start }
-    return { data: slice, cpositions: [0], dpositions: [0], chunk }
+    // chunk.minv = { dataPosition: range[0].start }
+    return { data: buffer, cpositions: [0], dpositions: [0], chunk: c2 }
   }
 
   async getHeader(opts: BaseOpts = {}) {
-    const url = `${this.baseUrl}/${this.trackId}?format=BAM&referenceName=1`
-    const result = await fetch(url, { ...opts })
+    const url = `${this.baseUrl}/${this.trackId}?referenceName=na&class=header`
+    const result = await fetch(url, opts)
     if (!result.ok) {
-      throw new Error(result.statusText)
+      throw new Error(`Failed to fetch ${result.statusText}`)
     }
     const data = await result.json()
-    const header = data.htsget.urls[0]
-    const buf = header.url.startsWith('data:')
-      ? Buffer.from(header.url.split(',')[1], 'base64')
-      : undefined
-
-    const uncba = await unzip(buf)
+    const uncba = await concat(data.htsget.urls, opts)
 
     if (uncba.readInt32LE(0) !== BAM_MAGIC) {
       throw new Error('Not a BAM file')
@@ -78,10 +97,6 @@ export default class Htsget extends BamFile {
     const headLen = uncba.readInt32LE(4)
 
     const headerText = uncba.toString('utf8', 8, 8 + headLen)
-    //const { chrToIndex, indexToChr } = await this._readRefSeqs(headLen + 8, 65535, opts)
-    // this.chrToIndex = chrToIndex
-    // this.indexToChr = indexToChr
-    //
     const samHeader = parseHeaderText(headerText)
 
     // use the @SQ lines in the header to figure out the
--- test/htsget.test.js
+++ test/htsget.test.js
@@ -1,7 +1,7 @@
 import { HtsgetFile } from '../src'
 
 xdescribe('htsspec htsget wtsi', () => {
-  it('loads volvox-sorted.bam', async () => {
+  it('wtsi', async () => {
     const ti = new HtsgetFile({
       baseUrl: 'https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger',
       baseUrl: 'ga4gh/sample/NA12878',
@@ -12,7 +12,7 @@ xdescribe('htsspec htsget wtsi', () => {
 })
 
 describe('htsspec dnanexus', () => {
-  it('loads volvox-sorted.bam', async () => {
+  it('dnanexus', async () => {
     const ti = new HtsgetFile({
       baseUrl: 'http://htsnexus.rnd.dnanex.us/v1/reads',
       trackId: 'BroadHiSeqX_b37/NA12878',
