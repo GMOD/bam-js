542f81397ec8183f382ad6a3143a9498d92caddd
Colin <colin.diesh@gmail.com>
2024-11-10 23:55:54 -0500

    Coverage

--- src/indexFile.ts
+++ src/indexFile.ts
@@ -1,10 +1,12 @@
 import { GenericFilehandle } from 'generic-filehandle'
+import VirtualOffset from './virtualOffset'
 import Chunk from './chunk'
 import { BaseOpts } from './util'
 
 export default abstract class IndexFile {
   public filehandle: GenericFilehandle
   public renameRefSeq: (s: string) => string
+  public setupP?: Promise<any>
 
   /**
    * @param {filehandle} filehandle
@@ -21,6 +23,7 @@ export default abstract class IndexFile {
     this.renameRefSeq = renameRefSeq
   }
   public abstract lineCount(refId: number): Promise<number>
+  protected abstract _parse(opts?: BaseOpts): Promise<any>
   public abstract indexCov(
     refId: number,
     start?: number,
@@ -33,4 +36,28 @@ export default abstract class IndexFile {
     end: number,
     opts?: BaseOpts,
   ): Promise<Chunk[]>
+
+  _findFirstData(data: any, virtualOffset: VirtualOffset) {
+    const currentFdl = data.firstDataLine
+    if (currentFdl) {
+      data.firstDataLine =
+        currentFdl.compareTo(virtualOffset) > 0 ? virtualOffset : currentFdl
+    } else {
+      data.firstDataLine = virtualOffset
+    }
+  }
+
+  async parse(opts: BaseOpts = {}) {
+    if (!this.setupP) {
+      this.setupP = this._parse(opts).catch(e => {
+        this.setupP = undefined
+        throw e
+      })
+    }
+    return this.setupP
+  }
+
+  async hasRefSeq(seqId: number, opts: BaseOpts = {}) {
+    return !!((await this.parse(opts)).indices[seqId] || {}).binIndex
+  }
 }
